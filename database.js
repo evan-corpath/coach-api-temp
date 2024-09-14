1cf89b920d3861bc3495121baa268f2697da2c66
Evan OShea <evanoshea21@gmail.com>
2024-09-13 22:56:23 -0700

    db localhost to 127.0.0.1

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 // Create the connection to database
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
   database: "sellness",
