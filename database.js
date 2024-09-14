3b91eaeaed467af1297e0c49a26c70e681d9ec58
Evan OShea <evanoshea21@gmail.com>
2024-09-13 23:06:25 -0700

    remote user

--- database.js
+++ database.js
@@ -4,8 +4,8 @@ const mysql = require("mysql2");
 // Create the connection to database
 const connection = mysql.createConnection({
   host: "127.0.0.1",
-  user: "root",
-  password: "",
+  user: "accessapi",
+  password: "pw",
   database: "sellness",
 });
 
